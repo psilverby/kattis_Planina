3ca1a81a176488f5400e19feb8eaa117d7115180
psilverby <87539667+psilverby@users.noreply.github.com>
2021-07-16 18:45:44 +0200

    Add files via upload

---
+++ Planina/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Planina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iterations = int.Parse(Console.ReadLine());
+            int points = 0;
+            int squares = 1;
+            for (int i = 0; i < iterations; i++)
+            {
+                squares = squares * 2;
+                int pointsInOneDimension = squares + 1;
+                points = pointsInOneDimension * pointsInOneDimension;
+            }
+
+            Console.WriteLine(points);
+        }
+    }
+}
